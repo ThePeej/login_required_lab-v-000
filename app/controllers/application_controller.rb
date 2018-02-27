05b7479c226efb6559032ff8045131437a326bc9
PJ Frias <peejfrias@gmail.com>
2018-02-27 18:03:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    if session.include? :user_id
+    if session.include? :name
+      session[]
   end
 end
